3423bf7d6516fd090ca0bb251fe34618304b67fb
Weilei Zeng <240155787@qq.com>
2020-02-27 14:55:13 -0800

    add python crawler scripts, not finish yet

---
+++ src/repo_data.py
@@ -0,0 +1,6 @@
+# use Github API to get the data of repos, including star_counts
+# do some post processing like statistics of all stars and all forks.
+
+# TODO:
+# use curl to download data for repo listes in _data
+# use python json lib to read and process data
