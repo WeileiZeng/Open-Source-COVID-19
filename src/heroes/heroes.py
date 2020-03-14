a40254d36243de7edca6d2cf6d37ff88d673f3c3
Divy Srivastava <dj.srivastava23@gmail.com>
2020-03-14 18:45:22 +0530

    Update heroes.py

--- src/heroes/heroes.py
+++ src/heroes/heroes.py
@@ -1,1 +1,12 @@
+import yaml
+import requests
 
+def save(api_content,username):
+    with  open( 'json_api/'+username.replace('/','.')+'.json','w') as f:
+        f.write(
+            json.dumps(api_content, sort_keys=True, indent=4)
+        )
+     
+def getUserInfo(user):
+  r = requests.get('https://api.github.com/user'+user)
+  return r
