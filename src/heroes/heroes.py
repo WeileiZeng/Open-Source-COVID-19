882afb362fe51b969a5f3784df327f40fa896c90
Divy Srivastava <dj.srivastava23@gmail.com>
2020-03-15 17:04:11 +0530

    A working script

--- src/heroes/heroes.py
+++ src/heroes/heroes.py
@@ -11,3 +11,14 @@ def getUserInfo(user):
   r = requests.get('https://api.github.com/user'+user)
   data = r.json()
   save(r, r.login)
+
+keyword="COVID"
+keyword="nCoV"
+keyword="covid19" # a lot of repos start with that name
+
+response=requests.get("https://api.github.com/search/repositories?q="+keyword+"&sort=stars&order=desc")
+
+for repo in response:
+  contributors = requests.get(repo.contributors_url)
+  for contributor in contributors:
+    getUserInfo(contributor)
